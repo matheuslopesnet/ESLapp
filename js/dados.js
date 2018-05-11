d3d68e3173f9a27ff46217d10b081413b15ea8d6
Matheus Lopes <mtlps25@gmail.com>
2018-05-10 21:53:19 -0300

    Start
    Random, ask and answer functions.

---
+++ js/dados.js
@@ -0,0 +1,5 @@
+var produtos = [
+        {"id":"1","ask":"Qual seu nome?","a1":"My name is John","a2":"My name are John","a3":"My name John","a4":"Me name is John","answer":"My name is John"},
+        {"id":"2","ask": "Qual sua idade?", "a1":"I is 22 years", "a2":"I am 22 years old", "a3":"My age are 22","a4":"I are 22 old","answer":"I am 22 years old"}
+]
+
